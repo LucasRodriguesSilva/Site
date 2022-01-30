ce903657996d3e0e8ebc0c501579fd6fd65fa80c
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-29 21:53:14 -0300

    Colocando algumas transições nos celulares

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var body = document.body;
+var h1 = document.createElement('h1');
+var texto = document.createTextNode("Boa Noite");
+h1.appendChild(texto);
+body.appendChild(h1);
+
+for (var i = 0; i < 10; i++) {
+    document.write
+}
